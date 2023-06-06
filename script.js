40571a68d623c0a9afe606139a5c83ee5b00268f
Papi <111683116+itsayopapi@users.noreply.github.com>
2023-06-06 11:55:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,19 @@
-// Toggle Login/Signup Form
 function toggleLoginForm() {
   var loginPopup = document.getElementById("login-popup");
-  loginPopup.style.display = (loginPopup.style.display === "block") ? "none" : "block";
+  loginPopup.classList.toggle("show");
 }
 
-// Form Validation
+function toggleSignupForm() {
+  var signupPopup = document.getElementById("signup-popup");
+  signupPopup.classList.toggle("show");
+}
+
 function validateLoginForm() {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-
-  if (email.trim() === "") {
-    alert("Please enter your email.");
-    return false;
-  }
-
-  if (password.trim() === "") {
-    alert("Please enter your password.");
-    return false;
-  }
-
-  return true;
+  // Add your login form validation logic here
+  return true; // Return false if validation fails and prevent form submission
 }
 
 function validateSignupForm() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-
-  if (name.trim() === "") {
-    alert("Please enter your name.");
-    return false;
-  }
-
-  if (email.trim() === "") {
-    alert("Please enter your email.");
-    return false;
-  }
-
-  if (password.trim() === "") {
-    alert("Please enter your password.");
-    return false;
-  }
-
-  return true;
+  // Add your sign-up form validation logic here
+  return true; // Return false if validation fails and prevent form submission
 }
-
-// Submit Form
-document.getElementById("login-form").addEventListener("submit", function(event) {
-  event.preventDefault();
-  if (validateLoginForm()) {
-    // Perform login logic here
-    alert("Logged in successfully!");
-  }
-});
-
-document.getElementById("signup-form").addEventListener("submit", function(event) {
-  event.preventDefault();
-  if (validateSignupForm()) {
-    // Perform signup logic here
-    alert("Signed up successfully!");
-  }
-});
